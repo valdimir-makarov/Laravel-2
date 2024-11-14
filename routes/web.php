260437e9d328c2a93ec310b8bc6378bf59af7491
valdimir-makarov <125736386+valdimir-makarov@users.noreply.github.com>
2024-11-15 00:53:51 +0600

    third

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/1200', function () {
     return view('helllo bro');
 });
+ Route::get('/1203',function(){
+
+    return view("bbubun2");
+ });
+Route::get('/12034',function(){
+
+    return view("bbubun2");
+ });
